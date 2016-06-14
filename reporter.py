2f515411913af8a94047d13af5f83f186155ab40
jslim18 <jslim18@gmail.com>
2016-06-14 16:31:11 +0800

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -9,7 +9,7 @@ class Reporter(db.Entity):
     first_name = Required(str)
     last_name = Optional(str)
     username = Optional(str)
-    reported = Set("Scammer")
+    reported = Set("Believer")
     created = Required(datetime.datetime, default=datetime.datetime.now)
 
     def __str__(self):
